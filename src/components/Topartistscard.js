dda5c44e4aa3a19e75354b2ec9bf0e2341d35c86
Ghazal-k99 <--global>
2024-10-09 23:04:53 +0300

    first commit

--- src/components/Topartistscard.js
+++ src/components/Topartistscard.js
@@ -1,6 +1,6 @@
 function Topartistscard(props) {
   return (
-    <div className="text-center bg-white bg-opacity-5 rounded-2xl p-4 shadow-lg h-24 w-full border-slate-300 border border-opacity-50 m-8 flex items-center ">
+    <div className="text-center bg-white bg-opacity-5 rounded-2xl p-4 shadow-lg h-24 w-full border-slate-500 border-opacity-30	 border-[1px] m-8 flex items-center ">
       <img src={props.image} alt="Artist Image" className="rounded-full w-16 h-16 mr-4" />
       <div  className="w-3/4">
         <h2 className="text-xl text-white font-bold m-8 ">{props.title}</h2>
